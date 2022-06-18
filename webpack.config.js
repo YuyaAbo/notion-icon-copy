519461810895f357c1e8bcf213124f9667c82425
YuyaAbo <b1011211@gmail.com>
2022-06-19 03:25:52 +0900

    Move webpack.config to root directory

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@ const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
     mode: "production",
     entry: {
-        content_script: path.resolve(__dirname, "..", "src", "content_script.ts"),
-        background: path.resolve(__dirname, "..", "src", "background.ts"),
+        content_script: path.resolve(__dirname, "src", "content_script.ts"),
+        background: path.resolve(__dirname, "src", "background.ts"),
     },
     output: {
-        path: path.join(__dirname, "../dist"),
+        path: path.join(__dirname, "dist"),
         filename: "[name].js",
     },
     resolve: {
